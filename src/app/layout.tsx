8f5946813bcffa240cf8c219a0d50746c33c3664
Juan Evangelista <juan.nascimento@lello.com.br>
2025-04-07 21:43:37 -0300

    fix: Favicon and delete chunck images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,15 @@ import type { Metadata } from 'next';
 import { Inter, Calistoga } from 'next/font/google';
 import './globals.css';
 import { twMerge } from 'tailwind-merge';
+import { Head } from 'next/document';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 const calistoga = Calistoga({ subsets: ['latin'], variable: '--font-serif', weight: ['400'] });
 
 export const metadata: Metadata = {
-	title: 'Juan Evangelista - Frontend Software Engineer',
-	description: 'Created with love, for all developers to know me better. Welcome!',
+	title: 'Juan Evangelista - Frontend Engineer',
+	description:
+		'Created with love, for all developers to know me better. Front-End Engineer | Specialist in JavaScript | TypeScript | React.js | Next.js | jQuery | Redux | Performance Optimization & SEO',
 };
 
 export default function RootLayout({
@@ -18,6 +20,18 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
+			<head>
+				<link
+					rel='icon'
+					href='/icon.svg'
+					className='text-gradients-blue-to-green'
+				/>
+				<link
+					rel='shortcut icon'
+					href='/icon.svg'
+					className='text-gradients-blue-to-green'
+				/>
+			</head>
 			<body
 				className={twMerge(
 					inter.variable,
